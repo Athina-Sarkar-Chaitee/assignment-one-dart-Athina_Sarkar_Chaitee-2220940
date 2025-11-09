84670da2cf1dcec25a6485baa41bcf294e6848af
Athina Sarkar <athinasarkarchaitee@Athinas-MacBook-Air.local>
2025-11-09 16:44:46 +0600

    ques3 done

--- question3.dart
+++ question3.dart
@@ -7,6 +7,7 @@ class BankAccount { // Savings/Checking
   // 2. Constructor:
   //    - Initialize all properties
   //    - Set initial balance to 0.0
+
   // TODO: Implement the constructor
   BankAccount(this.accountNumber, this.accountHolder, this.accountType) : balance = 0.0;
   // 1. Properties:
@@ -20,6 +21,8 @@ class BankAccount { // Savings/Checking
   // TODO: Implement the deposit method
   void deposit(double amount) {
     // TODO: Add the amount to balance
+    balance += amount; 
+    
   }
   
   //    - withdraw(double amount): Remove money from account (check for sufficient funds)
@@ -27,19 +30,33 @@ class BankAccount { // Savings/Checking
   void withdraw(double amount) {
     // TODO: Check for sufficient funds and subtract amount
     // TODO: Print error message if insufficient funds
+    
+    if (amount <= balance) {
+      balance -= amount; 
+      print('Withdrew $amount from $accountHolder\'s account.');
+  } else {
+      print('Insufficient funds');
+  }
+    
   }
   
   //    - getBalance(): Return current balance
   // TODO: Implement the getBalance method
   double getBalance() {
     // TODO: Return the current balance
-    return 0; // TODO: Replace with actual balance
+    return balance;  // TODO: Replace with actual balance
   }
   
   //    - displayAccountInfo(): Show account details
   // TODO: Implement the displayAccountInfo method
   void displayAccountInfo() {
     // TODO: Display account information
+    print('Account Info:');
+    print('Account Number: $accountNumber');
+    print('Account Holder: $accountHolder');
+    print('Account Type  : $accountType');
+    print('Balance : $balance');
+
   }
 }
 
@@ -51,7 +68,7 @@ void main() {
   //    - Handling insufficient funds scenario
   
   // TODO: Create 3 bank accounts
-  final var account1 = BankAccount('12345', 'Alice', 'Savings');
+  final  account1 = BankAccount('12345', 'Alice', 'Savings');
   final account2 = BankAccount('67890', 'Bob', 'Checking');
   final account3 = BankAccount('11111', 'Charlie', 'Savings');
   
